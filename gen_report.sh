a01f2b24543b1703109cb2a42c61ff18f5299fba
igor <89206574501@mail.ru>
2022-01-13 23:34:44 +0300

    исправил запуск сервера

--- gen_report.sh
+++ gen_report.sh
@@ -4,5 +4,5 @@ rm -rf ./allure-reports
 echo "generate report"
 allure generate --clean -o allure-reports/
 zip -r ./report.zip ./allure-reports/*
-python3 -m http.server --directory ./allure-reports/
+python3 -m http.server --directory ./allure-reports/ &
 google-chrome-stable http://0.0.0.0:8000/
