4bdc2ae935364ab5ac6624dbc37c4015b4ec6072
igor <89206574501@mail.ru>
2022-01-14 19:38:18 +0300

    добавил килл процесса, который висит на порту 8000

--- gen_report.sh
+++ gen_report.sh
@@ -4,5 +4,6 @@ rm -rf ./allure-reports
 echo "generate report"
 allure generate --clean -o allure-reports/
 zip -r ./report.zip ./allure-reports/*
+sudo lsof -t -i tcp:8000 | xargs kill -9
 python3 -m http.server --directory ./allure-reports/ &
 google-chrome-stable http://0.0.0.0:8000/
