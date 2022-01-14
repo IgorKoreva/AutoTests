f6c93b0048bbc50128e5c64a282a4b5c4b6ff484
igor <89206574501@mail.ru>
2022-01-14 22:17:48 +0300

    добавил историю

--- gen_report.sh
+++ gen_report.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 rm -rf ./allure-reports/*
 allure generate --clean -o allure-reports/
+rm -rf ./allure-results/history
+cp -r ./allure-reports/history ./allure-results/history
 zip -r ./report.zip ./allure-reports/*
 sudo lsof -t -i tcp:8000 | xargs kill -9
 python3 -m http.server --directory ./allure-reports/ &
