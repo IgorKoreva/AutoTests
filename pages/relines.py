e78405466dbec463996f0ec999dffb483885f129
igor <89206574501@mail.ru>
2022-01-19 22:35:47 +0300

    Добавили get_const для получения констант по сайту Добавили поиск элементов brands_menu_brands, brands_menu_models, elements,not_found Добавили тест поиск по Марка, Модель, Артикул Добавил тест поиска элементов на главной странице

--- pages/relines.py
+++ pages/relines.py
@@ -5,6 +5,23 @@ from pages.elements import WebElement
 from pages.elements import ManyWebElements
 
 
+def get_const(site):
+    const_dict = {
+        'https://relines.ru/': {
+            'search': (('Chery Fora Мотор стеклоочистителя', 2),
+                       ('Chery Fora Мотор стеклооddsadsasdчистителя', 0)),
+            'brands': [('Great Wall', 'Sailor', '2804200-B00')],
+        },
+        'https://stg.relines.ru/': {
+            'search': (('Chery Fora Мотор стеклоочистителя', 2),
+                       ('Chery Fora Мотор стеклооddsadsasdчистителя', 0)),
+            'brands': (('Great Wall', 'Sailor', '2804200-B00'),
+                       ('Faw', 'Besturn B50', '2804200-B00')),
+        }
+    }
+    return const_dict[site]
+
+
 class MainPage(WebPage):
 
     def __init__(self, web_driver, site_value, url=''):
@@ -14,14 +31,24 @@ class MainPage(WebPage):
     # note main page
     search = WebElement(id='query')
     search_button = WebElement(xpath='//*[@id="searchBtn"]')
+    search_brands_menu = WebElement(id='searchMenuBtn')
     products_list = ManyWebElements(
         xpath='//a[@class="product-list-item product-list-item_hover_active product-list-price-item__hover-wrapper"]')
     cart = WebElement(xpath='//a[@class="header__basket"]')
+    brands_menu_brands = ManyWebElements(xpath='//*[@id="searchMenu"]/div/ul/li[*]/div')
+    brands_menu_models = ManyWebElements(xpath='//*[@id="searchMenu"]/div/ul/li[*]/ul/li')
+
     car_brands = ManyWebElements(xpath='//a[@class="main-page-group__link"]')
     brand_name_h1 = WebElement(xpath='//h1[@class="catalog-header__title"]')
+    elements = ManyWebElements(xpath='//a[contains(@class, "link header-nav__link") or '
+                                     'contains(@class, "header__logo") or '
+                                     'contains(@class, "link footer__menu-link") or '
+                                     'contains(@class, "link footer-messengers__messenger") or '
+                                     'contains(@class, "link header__phone")]')
 
     # note cart
     item_cart = ManyWebElements(xpath='//*[@id="root"]/section/section/div/main/ul/li/section/a')
 
     # note product
     buy = WebElement(xpath='//button[@class="button button_big button_yellow product-info__button-buy"]')
+    not_found = WebElement(xpath='//h5[@class="search-page-no-results__text-typography"]')
--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,37 +1,34 @@
 import json
+import time
 
 import allure
 import pytest
 
 from pages.base import write_cookies, read_cookies
-from pages.relines import MainPage
+from pages.relines import MainPage, get_const
 
 
 @allure.severity(allure.severity_level.BLOCKER)
-@allure.story('Тестирование поиска')
+@allure.story('Поиск')
 @pytest.mark.smoke
-@pytest.mark.parametrize('search, result', (
-        ('Chery Fora Мотор стеклоочистителя', 2),
-        ('Chery Fora Мотор стеклооddsadsasdчистителя', 0),
-))
-def test_check_main_search(search, result, web_browser, site_value):
+def test_check_main_search(web_browser, site_value):
     """Проверяем поиск на главной странице."""
+    const = get_const(site_value)
+    for search, count in const['search']:
+        with allure.step("Открываем главную страницу"):
+            page = MainPage(web_browser, site_value)
 
-    with allure.step("Открываем главную страницу"):
-        page = MainPage(web_browser, site_value)
+        with allure.step(f'Вводим в поисковую строку "{search}"'):
+            page.search = search
+        with allure.step('Нажимаем кнопку "поиск"'):
+            page.search_button.click()
 
-    with allure.step(f'Вводим в поисковую строку "{search}"'):
-        page.search = search
-    with allure.step('Нажимаем кнопку "поиск"'):
-        page.search_button.click()
-
-    # Verify that user can see the list of products:
-    with allure.step(f'проверяем что элементов "{result}"'):
-        assert page.products_list.count() == result
+        with allure.step(f'проверяем что элементов "{count}"'):
+            assert page.products_list.count() == count
 
 
 @allure.story('Тестирование корзины')
-@pytest.mark.acceptance
+@pytest.mark.smoke
 def test_put_in_cart(web_browser, site_value):
     """Добавляем один элемент в корзину."""
 
@@ -54,7 +51,7 @@ def test_put_in_cart(web_browser, site_value):
 
 
 @allure.story('Тестирование корзины')
-@pytest.mark.acceptance
+@pytest.mark.smoke
 def test_view_in_cart(web_browser, site_value):
     """Подкладываем куку и проверяем что есть 1 элемент в корзине."""
 
@@ -91,3 +88,60 @@ def test_open_catalog(web_browser, site_value):
 
         with allure.step('нажимаем назад'):
             page.go_back()
+
+
+@allure.severity(allure.severity_level.BLOCKER)
+@allure.feature('Другие')
+@allure.story('Главная старница')
+@pytest.mark.smoke
+def test_main_elements(web_browser, site_value):
+    """Наличие всех элементов на странице."""
+
+    with allure.step('Переходим на главную страницу'):
+        page = MainPage(web_browser, site_value)
+
+    with allure.step('Проверяем что на странице есть 9 элементов[лого, Каталог, Доставка, Контакты, телефон, оферта, телеграм, васап]'):
+        assert page.elements.count() == 9
+
+
+@allure.severity(allure.severity_level.BLOCKER)
+@allure.story('Поиск')
+@pytest.mark.underconstruction
+@pytest.mark.xfail(reason='https://fat.relines.ru/browse/WEB-1610')
+@allure.testcase('https://app.qase.io/project/WEBQ?view=1&suite=1&case=7&step=15', 'кейс, шаг 15')
+def test_search_brandart(web_browser, site_value):
+    """Поиск по марке/модели и артикулу"""
+
+    const = get_const(site_value)
+
+    for _brand, _model, _vendor_code in const['brands']:
+        with allure.step('Переходим на главную страницу'):
+            page = MainPage(web_browser, site_value)
+
+        with allure.step('Щелкаем в строку поиск'):
+            page.search.click()
+
+        with allure.step('Нажимаем меню "Все марки"'):
+            page.search_brands_menu.click()
+
+        with allure.step(f'Выбираем марку "{_brand}" модель "{_model}"'):
+            for brand in page.brands_menu_brands:
+                if _brand in brand.text:
+                    brand.click()
+                    for model in page.brands_menu_models:
+                        if _model in model.text:
+                            model.click()
+
+        with allure.step(f'Вводим в поисковую строку "{_vendor_code}"'):
+            page.search = _vendor_code
+
+        with allure.step('Нажимаем кнопку "поиск"'):
+            page.search_button.click()
+
+        with allure.step('Проверяем что найдена деталь'):
+            assert 'Ничего не найдено' != page.not_found.get_text()
+            assert 0 < page.products_list.count()
+
+
+# @pytest.mark.underconstruction
+# def test_
